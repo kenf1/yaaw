9fb9aded4ca586d69dd33a9969b3e65e0a962cd0
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-07-29 16:37:39 -0700

    Use module

--- random_text/src/index.js
+++ random_text/src/index.js
@@ -1,35 +1,4 @@
-const core = require("@actions/core");
-
-function getLength() {
-  const input = core.getInput("length");
-  const length = parseInt(input, 10);
-
-  if (isNaN(length)) {
-    throw new Error(
-      `Invalid input: length ('${input}') is not a valid number.`,
-    );
-  }
-  if (length < 1 || length > 50) {
-    throw new Error(
-      `Invalid input: length must be between 1 and 50. Provided: ${length}`,
-    );
-  }
-
-  return length;
-}
-
-function genRandomText(length) {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters[randomIndex];
-  }
-
-  return result;
-}
+const { getLength, genRandomText } = require("./logic");
 
 const randomText = genRandomText(getLength());
 console.log(randomText);
---
+++ random_text/src/logic.js
@@ -0,0 +1,34 @@
+const core = require("@actions/core");
+
+function getLength() {
+  const input = core.getInput("length");
+  const length = parseInt(input, 10);
+
+  if (isNaN(length)) {
+    throw new Error(
+      `Invalid input: length ('${input}') is not a valid number.`,
+    );
+  }
+  if (length < 1 || length > 50) {
+    throw new Error(
+      `Invalid input: length must be between 1 and 50. Provided: ${length}`,
+    );
+  }
+
+  return length;
+}
+
+function genRandomText(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+
+  return result;
+}
+
+module.exports = { getLength, genRandomText };
